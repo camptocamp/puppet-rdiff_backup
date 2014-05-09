7d1f8c4f8cb67beb3dd5384259453f5496f1cc95
Mickaël Canévet <mickael.canevet@camptocamp.com>
2014-05-09 13:25:49 +0200

    Fix with tests when using strict_variables

--- spec/classes/rdiff_backup__server_spec.rb
+++ spec/classes/rdiff_backup__server_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe 'rdiff_backup::server' do
   let(:facts) {{
-    :concat_basedir => '/foo',
-    :osfamily       => 'Debian',
+    :concat_basedir  => '/foo',
+    :id              => 'root',
+    :kernel          => 'Linux',
+    :operatingsystem => 'Debian',
+    :osfamily        => 'Debian',
+    :path            => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
   }}
   it { should compile.with_all_deps }
 end
